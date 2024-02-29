f15b3297bbb5c877f0bd4efb6465610099365d7f
griffincj <cjg95@pitt.edu>
2024-02-29 08:43:43 -0500

    Update README.md and example from homework

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Power-System-Analysis
 This is the project for the ECE2774 project
+
+For milestone 1, ``main.py`` contains all initializations of each instance of the 
+classes required for the project model. By running ``main.py``, 7 buses, 6 transmission lines,
+and 2 transformers are created.
+
+The ``main.py`` method will print the sample transformer and transmission line impedance and admittance
+(and total shunt for transmission line) from HW1.
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-
 from transformer import Transformer
 from transmission_line import TransmissionLine
 from transmission_line import Conductor, ConductorBundle
 from bus import Bus
 from power_system import PowerSystem
-
 
 if __name__ == '__main__':
     # Initialize Power System instance
@@ -23,14 +21,13 @@ if __name__ == '__main__':
     ps.add_bus(bus_5)
     bus_6 = Bus(voltage_base=230, bus_name="6")
     ps.add_bus(bus_6)
-    load = Bus(voltage_base=18, bus_name="7")
-
-    ps.add_bus(load)
+    gen2 = Bus(voltage_base=18, bus_name="7")
+    ps.add_bus(gen2)
 
     # Transformer low-side, high-side is based on the buses connected on either side of it
     t1 = Transformer(rating=125, bus_a=bus_1, bus_b=bus_2, z_pu=0.085, x_r_ratio=10)
     ps.add_transformer(t1)
-    t2 = Transformer(rating=200, bus_a=bus_6, bus_b=load, z_pu=0.105, x_r_ratio=12)
+    t2 = Transformer(rating=200, bus_a=bus_6, bus_b=gen2, z_pu=0.105, x_r_ratio=12)
     ps.add_transformer(t2)
 
     # Initialize base conductor and bundle, used by each transmission line
@@ -42,6 +39,7 @@ if __name__ == '__main__':
     # Calculated distances
     distances = [19.5, 19.5, 39]
 
+    # Add transmission lines
     tl1 = TransmissionLine(conductor_bundle=bundle, distances=distances, length=10, bus_a=bus_2, bus_b=bus_4)
     tl2 = TransmissionLine(conductor_bundle=bundle, distances=distances, length=25, bus_a=bus_2, bus_b=bus_3)
     tl3 = TransmissionLine(conductor_bundle=bundle, distances=distances, length=20, bus_a=bus_3, bus_b=bus_5)
@@ -56,19 +54,21 @@ if __name__ == '__main__':
     ps.add_transmission_line(tl6)
     ps.calculate_y_bus()
 
-    '''
-    # Homework example
-    hw_bus = Bus(voltage_base=500, bus_name="A")
+    ####################
+    # Homework example #
+    ####################
+    hw_busA = Bus(voltage_base=20, bus_name="A")
+    hw_busB = Bus(voltage_base=500, bus_name="B")
     finch_conductor = Conductor(gmr=0.0435, r_per_mile=0.0969, out_diameter=1.293)
     finch_bundle = ConductorBundle(conductor=finch_conductor, num_conductors=3, conductor_distance=1)
+    hw_xfmr = Transformer(rating=500, bus_a=hw_busA, bus_b=hw_busB, z_pu=0.075, x_r_ratio=13)
     hw_tl = TransmissionLine(conductor_bundle=finch_bundle, distances=[12.5, 12.5, 20],
-                             length=100, bus_a=hw_bus, bus_b=hw_bus)
+                             length=100, bus_a=hw_busB, bus_b=hw_busB)
+    print("###########\nSAMPLE OUTPUT (from Homework)\n###########\n")
+    print("HW XFMR Impedance PU: " + str(hw_xfmr.z_pu_sys))
+    print("HW XFMR Admittance PU: " + str(hw_xfmr.yt_pu) + "\n")
 
-    
-    print("HW TL Deq: " + str(hw_tl.deq))
-    print("HW TL Reactance: " + str(hw_tl.x))
-    print("HW TL Susceptance: " + str(hw_tl.b))
-    print("HW TL Resistance: " + str(hw_tl.r))
-    print("HW TL Impedance: " + str(hw_tl.z))
-    print("HW TL Admittance: " + str(hw_tl.ytl_pu))
-    '''
+    print("HW TL Impedance PU: " + str(hw_tl.z))
+    print("HW TL Susceptance PU: " + str(hw_tl.b))
+    print("HW TL Admittance PU: " + str(hw_tl.ytl_pu))
+
